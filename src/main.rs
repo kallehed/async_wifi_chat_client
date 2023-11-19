3b0e134ee41f240b4649881fae72807021d43705
kallehed <kallehed@gmail.com>
2023-11-19 11:58:07 +0100

    write names and fix STDIN getting used multiple times with same thing

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,8 @@
 use std::collections::HashMap;
 use std::net::IpAddr;
+use std::sync::atomic::AtomicBool;
 use std::sync::Arc;
 use std::sync::Mutex;
-use std::sync::atomic::AtomicBool;
 use std::{sync::atomic::Ordering, time::Duration};
 use tokio::{
     io::{AsyncReadExt, AsyncWriteExt},
@@ -18,6 +18,21 @@ const LISTEN_SOCKET: (&str, u16) = (LISTEN_ADDRESS, PORT);
 
 static IN_CONNECTION: AtomicBool = AtomicBool::new(false);
 
+static NEW_STDIN: AtomicBool = AtomicBool::new(false);
+static STDIN_MSG: Mutex<StdinMsg> = Mutex::new([0; 32]);
+
+/// wait until new message and return it, while marking it as not new
+async fn get_new_stdin_msg() -> StdinMsg {
+    loop {
+        if !NEW_STDIN.load(Ordering::Relaxed) {
+            tokio::time::sleep(Duration::from_millis(100)).await;
+            continue;
+        }
+        NEW_STDIN.store(false, Ordering::Relaxed);
+        return *STDIN_MSG.lock().unwrap();
+    }
+}
+
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let mut name: StdinMsg = [0; 32];
@@ -27,19 +42,14 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     tokio::spawn(listen_for_peers(peers.clone(), name));
     tokio::spawn(print_available_peers(peers.clone()));
 
-    let (stdin_watch_sender, stdin_watch_receiver) = tokio::sync::watch::channel([0; 32]);
-
     tokio::spawn(async move {
-        stdin_listener(stdin_watch_sender).await.unwrap();
-    });
-    let stdin_watch_receiver_clone = stdin_watch_receiver.clone();
-    tokio::spawn(async move {
-        tcp_connection_listener(stdin_watch_receiver_clone)
-            .await
-            .unwrap();
+        stdin_listener().await.unwrap();
     });
     tokio::spawn(async move {
-        tcp_connection_creator(stdin_watch_receiver, peers.clone()).await;
+        tcp_connection_listener().await.unwrap();
+    });
+    tokio::spawn(async move {
+        tcp_connection_creator(peers.clone(), name).await;
     });
     loop {}
 }
@@ -66,18 +76,14 @@ async fn print_available_peers(peers: Arc<Mutex<HashMap<IpAddr, StdinMsg>>>) {
 }
 
 // receives from STDIN and tries to create connections
-async fn tcp_connection_creator(
-    mut stdin_watch_receiver: tokio::sync::watch::Receiver<StdinMsg>,
-    peers: Arc<Mutex<HashMap<IpAddr, StdinMsg>>>,
-) {
+async fn tcp_connection_creator(peers: Arc<Mutex<HashMap<IpAddr, StdinMsg>>>, my_name: StdinMsg) {
     loop {
         if IN_CONNECTION.load(Ordering::Relaxed) {
             tokio::time::sleep(Duration::from_millis(500)).await;
             continue;
         }
-        stdin_watch_receiver.changed().await.unwrap();
         let num = {
-            let inp = &*stdin_watch_receiver.borrow_and_update();
+            let inp = get_new_stdin_msg().await;
             if IN_CONNECTION.load(Ordering::Relaxed) {
                 continue;
             }
@@ -88,73 +94,62 @@ async fn tcp_connection_creator(
                 .unwrap_or(inp.len());
             let inp = &inp[..len];
             let inp = std::str::from_utf8(inp).unwrap_or("ERRENOUS UTF8");
-            println!("you just said: {:?}", inp);
             let Ok(num) = inp.parse::<u16>() else {
                 println!("ERROR: {:?} is not a number", inp);
                 continue;
             };
             num
         };
-        let peer = {
+        let (peer, peer_name) = {
             let set = peers.lock().unwrap();
 
             let Some(peer) = set.iter().skip(num as _).next() else {
                 println!("ERROR: no such peer exists!");
                 continue;
             };
-            *peer.clone().0
+            (*peer.clone().0, *peer.1)
         };
-        let Ok(tcp_stream) = tokio::net::TcpStream::connect((peer, PORT)).await else {
+        let Ok(mut tcp_stream) = tokio::net::TcpStream::connect((peer, PORT)).await else {
             println!("ERROR: Could not connect!");
             continue;
         };
-        spawn_connection_from(tcp_stream, stdin_watch_receiver.clone()).await;
+        tcp_stream.write_all(&my_name).await.unwrap();
+        spawn_connection_from(tcp_stream, peer_name).await;
     }
 }
 
-async fn tcp_connection_listener(
-    stdin_watch_receiver: tokio::sync::watch::Receiver<StdinMsg>,
-) -> Result<(), Box<dyn std::error::Error>> {
+async fn tcp_connection_listener() -> Result<(), Box<dyn std::error::Error>> {
     let tcp = TcpListener::bind(LISTEN_SOCKET).await?;
     loop {
-        let (stream, addr) = tcp.accept().await?;
+        let (mut stream, addr) = tcp.accept().await?;
         println!("got connection from: {:?}", addr);
+        let mut buf = [0;32];
+        stream.read(&mut buf).await.unwrap();
 
-        spawn_connection_from(stream, stdin_watch_receiver.clone()).await;
+        spawn_connection_from(stream, buf).await;
     }
 }
 
-async fn spawn_connection_from(
-    stream: tokio::net::TcpStream,
-    stdin_watch_receiver: tokio::sync::watch::Receiver<StdinMsg>,
-) {
+async fn spawn_connection_from(stream: tokio::net::TcpStream, peer_name: StdinMsg) {
     if IN_CONNECTION.load(Ordering::Relaxed) {
-        println!("BUT already in connection");
+        println!("BUT already in connection!");
         return;
     }
     IN_CONNECTION.store(true, Ordering::Relaxed);
-    println!("CONNECTION CREATED!");
+    println!(
+        "CONNECTION CREATED WITH: {:?}!",
+        stream.peer_addr().unwrap()
+    );
     let (read_half, write_half) = stream.into_split();
     let atom = Arc::new(AtomicBool::new(false));
-    tokio::spawn(tcp_connection_receiver(read_half, atom.clone()));
-    tokio::spawn(tcp_connection_writer(
-        write_half,
-        stdin_watch_receiver.clone(),
-        atom,
-    ));
+    tokio::spawn(tcp_connection_receiver(read_half, atom.clone(), peer_name));
+    tokio::spawn(tcp_connection_writer(write_half, atom));
 }
 
-async fn tcp_connection_writer(
-    mut write: tokio::net::tcp::OwnedWriteHalf,
-    mut stdin_watch_receiver: tokio::sync::watch::Receiver<StdinMsg>,
-    done: Arc<AtomicBool>,
-) {
+async fn tcp_connection_writer(mut write: tokio::net::tcp::OwnedWriteHalf, done: Arc<AtomicBool>) {
     loop {
-        stdin_watch_receiver.changed().await.unwrap();
         let mut my_buf = [0; 32];
-        stdin_watch_receiver
-            .borrow_and_update()
-            .clone_into(&mut my_buf);
+        get_new_stdin_msg().await.clone_into(&mut my_buf);
         if done.load(Ordering::Relaxed) {
             break;
         }
@@ -171,14 +166,10 @@ async fn tcp_connection_writer(
         done.store(true, Ordering::Relaxed);
         IN_CONNECTION.store(false, Ordering::Relaxed);
     }
-    // println!("exit tcp sender");
 }
 
 /// read from TcpListener and print to STDOUT
-async fn tcp_connection_receiver(
-    mut read: tokio::net::tcp::OwnedReadHalf,
-    done: Arc<AtomicBool>,
-) {
+async fn tcp_connection_receiver(mut read: tokio::net::tcp::OwnedReadHalf, done: Arc<AtomicBool>, peer_name: StdinMsg) {
     let mut stdout = tokio::io::stdout();
     loop {
         let mut buf = [0; 256];
@@ -192,26 +183,25 @@ async fn tcp_connection_receiver(
         if done.load(Ordering::Relaxed) {
             break;
         }
+        stdout.write_all(&peer_name).await.unwrap();
+        stdout.write_all(b": ").await.unwrap();
         stdout.write_all(&buf).await.unwrap();
     }
     if !done.load(Ordering::Relaxed) {
         done.store(true, Ordering::Relaxed);
         IN_CONNECTION.store(false, Ordering::Relaxed);
     }
-    // println!("exit tcplistener");
 }
 
-async fn stdin_listener(
-    watch: tokio::sync::watch::Sender<StdinMsg>,
-) -> Result<(), Box<dyn std::error::Error>> {
+async fn stdin_listener() -> Result<(), Box<dyn std::error::Error>> {
     println!("Listening on STDIN");
 
     let mut stdin = tokio::io::stdin();
-
     loop {
         let mut buf = [0; 32];
         stdin.read(&mut buf).await?;
-        watch.send(buf)?;
+        buf.clone_into(&mut STDIN_MSG.lock().unwrap());
+        NEW_STDIN.store(true, Ordering::Relaxed);
     }
 }
 
